02a2964289f2b2e1b265dbc51212cdb2b4132017
Misty - NT <166186521+Misty-NT@users.noreply.github.com>
2025-08-13 08:30:57 -0700

    Create AD Password History.ps1

---
+++ AD Password History.ps1
@@ -0,0 +1,9 @@
+# Get all enabled users, with the Last Password Set date
+Get-ADUser -Filter * -Properties DisplayName, SamAccountName, PasswordLastSet |
+    Select-Object DisplayName,
+                  SamAccountName,
+                  @{Name="PasswordLastChanged"; Expression={($_.PasswordLastSet).ToLocalTime()}} |
+    Sort-Object PasswordLastChanged -Descending |
+    Export-Csv -Path "C:\Temp\AD_UserPasswordLastChanged.csv" -NoTypeInformation
+
+Write-Host "✅ Export complete. File saved to C:\NetworkTitan\AD_UserPasswordLastChanged.csv"

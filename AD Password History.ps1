d9df865a3001c9c844db1f86bc267ffa87b36fde
Misty - NT <166186521+Misty-NT@users.noreply.github.com>
2025-08-13 08:35:17 -0700

    Update AD Password History.ps1

--- AD Password History.ps1
+++ AD Password History.ps1
@@ -6,4 +6,4 @@ Get-ADUser -Filter * -Properties DisplayName, SamAccountName, PasswordLastSet |
     Sort-Object PasswordLastChanged -Descending |
     Export-Csv -Path "C:\NetworkTitan\AD_UserPasswordLastChanged.csv" -NoTypeInformation
 
-Write-Host "✅ Export complete. File saved to C:\NetworkTitan\AD_UserPasswordLastChanged.csv"
+Write-Host "Export complete. File saved to C:\NetworkTitan\AD_UserPasswordLastChanged.csv"
